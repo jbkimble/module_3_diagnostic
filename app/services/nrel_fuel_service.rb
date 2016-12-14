81dd71a818207f323e72c2bc691b6447f5e761e9
Jon Kimble <jonathanbkimble@gmail.com>
2016-12-14 11:04:45 -0700

    Extract API call to service

--- app/services/nrel_fuel_service.rb
+++ app/services/nrel_fuel_service.rb
@@ -1,10 +1,10 @@
 class NrelFuelService
 
   def fuel_stations(zipcode)
-    uri = URI("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{params['q']}&status=E&fuel_type=LPG,ELEC&api_key=JMD70usgOO1tUrJtyJKDSTb2B2yHdKMbq4SOfj3S")
+    uri = URI("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{zipcode}&status=E&fuel_type=LPG,ELEC&api_key=JMD70usgOO1tUrJtyJKDSTb2B2yHdKMbq4SOfj3S")
     response = Net::HTTP.get(uri)
     parsed_json = JSON.parse(response, :symbolize_names => true)
-    @fuel_stations =  NrelFuel.top_10_stations(parsed_json)
+    parsed_json[:fuel_stations][0..9]
   end
 
 end
