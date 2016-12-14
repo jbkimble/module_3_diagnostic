a2a78eee0dedcea525e4e4afa6f6faf89324a04b
Jon Kimble <jonathanbkimble@gmail.com>
2016-12-14 09:48:58 -0700

    Add test for user locating nearest stations

--- spec/features/user_can_locate_closest_stations_spec.rb
+++ spec/features/user_can_locate_closest_stations_spec.rb
@@ -8,9 +8,24 @@ describe 'As a user' do
       click_on "Locate"
 
       expect(current_path).to eq(search_path)
+      expect(page).to have_content("UDR")
+      expect(page).to have_content("800 Acoma St")
+      expect(page).to have_content("ELEC")
+      expect(page).to have_content("0.3117")
+      expect(page).to have_content("24 hours daily")
 
+      expect(page).to have_content("PUBLIC STATIONS")
+      expect(page).to have_content("1370 Elati St")
+      expect(page).to have_content("ELEC")
+      expect(page).to have_content("0.75983")
+      expect(page).to have_content("24 hours daily")
+
+      expect(page).to_not have_content("BD")
+      expect(page).to_not have_content("CNG")
+      expect(page).to_not have_content("E85")
       # I see 10 closest stations sorted by distance
       # The stations are limited to electric & propane
+      #for each station I see name, address, fuel types, distance, and access times
     end
   end
 end
