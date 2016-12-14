10f26bebdf45ca681b4c5b1a308afc5c161f0bb5
Jon Kimble <jonathanbkimble@gmail.com>
2016-12-14 11:03:32 -0700

    Refactor out api call to service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-    NrelFuel.top_10_stations(params['q'])
-
-    # uri = URI("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?location=#{params['q']}&status=E&fuel_type=LPG,ELEC&api_key=JMD70usgOO1tUrJtyJKDSTb2B2yHdKMbq4SOfj3S")
-    # response = Net::HTTP.get(uri)
-    # parsed_json = JSON.parse(response, :symbolize_names => true)
-    # @fuel_stations =  NrelFuel.top_10_stations(parsed_json)
+    @fuel_stations = NrelFuel.top_10_stations(params['q'])
   end
 end
