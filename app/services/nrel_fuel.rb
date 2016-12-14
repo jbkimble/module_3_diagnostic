858752ff3302660eaf536577d7bce5d0bebb0dd5
Jon Kimble <jonathanbkimble@gmail.com>
2016-12-14 11:03:54 -0700

    Add attr_readers

--- app/services/nrel_fuel.rb
+++ app/services/nrel_fuel.rb
@@ -1,15 +1,17 @@
 class NrelFuel
+  attr_reader :name, :address, :fuel_type, :distance, :access_times
   def initialize(attributes={})
       @name = attributes[:station_name]
-      @address = attributes[:station_name]
-      @fuel_type = attributes[:station_name]
-      @distance = attributes[:station_name]
-      @access_times = attributes[:station_name]
+      @address = attributes[:street_address]
+      @fuel_type = attributes[:fuel_type_code]
+      @distance = attributes[:distance]
+      @access_times = attributes[:access_days_time]
   end
 
-
-  def self.top_10_stations(parsed_json)
-    parsed_json[:fuel_stations][0..9]
+  def self.top_10_stations(zipcode)
+    NrelFuelService.new.fuel_stations(zipcode).map do |station|
+      NrelFuel.new(station)
+    end
   end
-  
+
 end
